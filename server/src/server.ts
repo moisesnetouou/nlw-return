74daa746b7b7168a9a25e2f3bdee73fdf5897f54
Moisés <moisesnetored@gmail.com>
2022-05-04 17:36:22 -0400

    feat: Email sending, in development.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,43 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (req, res)=> {
-  return res.send('Hello World');
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "user",
+    pass: "pass"
+  }
+});
+
+app.post('/feedbacks', async (req, res)=> {
+  const {type, comment, screenshot} = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+  
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Moisés Neto <teste@email.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</>`,
+      `<p>Comentário: ${comment}</>`,
+      `</div>`
+    ].join('\n')
+  });
+
+  return res.status(201).json({data: feedback})
 });
 
 app.listen(3333, () => {
