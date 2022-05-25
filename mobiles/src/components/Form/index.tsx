5d8d4df8a522b096f262dfae8f3569f2d3f287a0
Moisés <moisesnetored@gmail.com>
2022-05-25 08:41:13 -0400

    feat: Mobile components adjusted.

---
+++ mobiles/src/components/Form/index.tsx
@@ -0,0 +1,68 @@
+import { ArrowLeft } from 'phosphor-react-native';
+import React from 'react';
+import { 
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Text
+} from 'react-native';
+
+import {FeedbackType} from '../../components/Widget';
+import {feedbackTypes} from '../../utils/feedbackTypes';
+
+import { theme } from '../../theme';
+
+import { styles } from './styles';
+import { ScreenshotButton } from '../ScreenshotButton';
+import { Button } from '../Button';
+
+interface Props {
+  feedbackType: FeedbackType;
+}
+
+export function Form({feedbackType}: Props) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <ArrowLeft 
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.titleContainer}>
+          <Image 
+            source={feedbackTypeInfo.image}
+            style={styles.image}
+          />
+          <Text style={styles.titleText}>
+            {feedbackTypeInfo.title}
+          </Text>
+        </View>
+      </View>
+
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem ? Queremos corrigir. Conter com
+        detalhes o que está acontecendo..."
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+
+      <View style={styles.footer}>
+        <ScreenshotButton 
+          onTakeShot={()=>{}}
+          onRemoveShot={()=> {}}
+          screenshot="https://github.com/moisesnetouou.png"
+        />
+
+        <Button isLoading={false} />
+      </View>
+    </View>
+  );
+}
